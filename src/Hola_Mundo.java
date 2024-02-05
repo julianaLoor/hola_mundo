04f02522b7b949343490a01c7f5da748c5513e24
julianaLoor <julianalisseth@gmail.com>
2024-02-05 14:08:23 +0100

    Commit 2

--- src/Hola_Mundo.java
+++ src/Hola_Mundo.java
@@ -4,6 +4,7 @@ public class Hola_Mundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Que tal");
+		System.out.println("Nuevo commit");
 	}
 
 }
